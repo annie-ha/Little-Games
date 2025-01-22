3af4c29d0c7a45359d3b763edd4410cdddb1af74
annie-ha <annieha@annies-imac.home>
2025-01-22 21:43:22 +1100

    set highscore

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -8,6 +8,7 @@ let secretNumber = Math.trunc(Math.random() * 20 + 1);
 // document.querySelector(".number").textContent = secretNumber;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -22,6 +23,12 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    // Set Highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     // When guess is too high
     if (score > 1) {
