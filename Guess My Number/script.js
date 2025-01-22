2230ccfc7b680f547c8f6ccf4fe4044056118057
annie-ha <annieha@annies-imac.home>
2025-01-22 20:38:07 +1100

    adding secret number and evaluation

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -2,7 +2,12 @@
 
 // document.querySelector(".message").textContent = "Correct Answer!";
 // document.querySelector(".guess").value;
-document.querySelector(".score").textContent = 10;
+// document.querySelector(".score").textContent = 10;
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".number").textContent = secretNumber;
+
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -10,5 +15,27 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = `Correct Number! You Win!`;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
   }
 });
