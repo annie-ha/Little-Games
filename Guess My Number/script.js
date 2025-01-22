2c714fc022f84deca5b7e0f978dd65dfea555910
annie-ha <annieha@annies-imac.home>
2025-01-22 19:24:30 +1100

    adding Event Listerner

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+
+// document.querySelector(".message").textContent = "Correct Answer!";
+// document.querySelector(".guess").value;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+  }
+});
